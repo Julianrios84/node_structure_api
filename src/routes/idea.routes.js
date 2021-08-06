142fa61fc62174259ff9d822edc91b65cd04f43d
Julian Esteban Rios Holguin <desarrolladorjr4.spv@gmail.com>
2021-08-05 22:00:31 -0500

    [ADD] swagger implemented

--- src/routes/idea.routes.js
+++ src/routes/idea.routes.js
@@ -1,17 +1,19 @@
 const { Router } = require("express");
-const { ParseIntMiddleware } = require('../middlewares')
+const { AuthMiddleware, ParseIntMiddleware, CacheMiddleware } = require('../middlewares')
+
+const { CACHE_TIME } = require('../helpers');
 
 module.exports = function ({ IdeaController }) {
   const router = Router();
 
-  router.get("", ParseIntMiddleware, IdeaController.getAll);
+  router.get("", [ParseIntMiddleware, CacheMiddleware(CACHE_TIME.ONE_HOUR)], IdeaController.getAll);
   router.get("/:ideaId", IdeaController.get);
   router.get("/:userId/all", IdeaController.getUserIdeas);
-  router.post("", IdeaController.create);
-  router.patch("/:ideaId", IdeaController.update);
-  router.delete("/:ideaId", IdeaController.delete);
-  router.post("/:ideaId/upvote", IdeaController.upvoteIdea);
-  router.post("/:ideaId/downvote", IdeaController.downvoteIdea);
+  router.post("", AuthMiddleware, IdeaController.create);
+  router.patch("/:ideaId", AuthMiddleware, IdeaController.update);
+  router.delete("/:ideaId", AuthMiddleware, IdeaController.delete);
+  router.post("/:ideaId/upvote", AuthMiddleware, IdeaController.upvoteIdea);
+  router.post("/:ideaId/downvote", AuthMiddleware, IdeaController.downvoteIdea);
 
   return router;
 };
